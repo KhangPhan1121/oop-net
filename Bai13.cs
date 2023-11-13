bf87ce37860192a20d5a44a0271ac24218de2965
LAPTOP-G4JJ8UTM\Phan_Hoang_Khang <KhangPhan1121>
2023-11-13 22:31:51 +0700

    Add

--- Bai13.cs
+++ Bai13.cs
@@ -17,7 +17,7 @@ namespace netdemo
         {
             get
             {
-
+                // Trả về số điện thoại với dấu "+" đằng trước
                 return "+" + _Phone;
             }
             set
@@ -39,6 +39,8 @@ namespace netdemo
             EmployeeType = employeeType;
             EmployeeCount = employeeCount;
         }
+
+        // Phương thức hiển thị thông tin của nhân viên
         public virtual void ShowInfo()
         {
             Console.WriteLine($"ID {ID}");
@@ -53,7 +55,9 @@ namespace netdemo
         public bool IsValidBirthDay(int birthDay)
         {
             int currentYear = DateTime.Now.Year;
-            return birthDay >= 1900 && birthDay <= currentYear;
+            int minBirthYear = 1900;
+
+            return birthDay >= minBirthYear && birthDay <= currentYear;
         }
         public bool IsValidEmail(string email)
         {
@@ -81,6 +85,8 @@ namespace netdemo
             ExpInYear = expInYear;
             ProSkill = proSkill;
         }
+
+        // Phương thức hiển thị thông tin của nhân viên có kinh nghiệm
         public override void ShowInfo()
         {
             base.ShowInfo();
@@ -101,6 +107,7 @@ namespace netdemo
             Education = education;
         }
 
+         // Phương thức hiển thị thông tin của nhân viên mới ra trường
         public override void ShowInfo()
         {
             base.ShowInfo();
@@ -121,6 +128,8 @@ namespace netdemo
             SemeSter = semeSter;
             UniversityName = universityName;
         }
+
+        // Phương thức hiển thị thông tin của nhân viên thực tập
         public override void ShowInfo()
         {
             base.ShowInfo();
@@ -147,7 +156,9 @@ namespace netdemo
     class BoPhanTuyenDung
     {
         public List<Certificate> certificates = new List<Certificate>();
-        public static List<Employee> employees = new List<Employee>();
+        public List<Employee> employees = new List<Employee>();
+
+       // Thêm chứng chỉ mới 
         public void AddCertificate(Certificate certificate)
         {
             certificates.Add(certificate);
@@ -159,6 +170,20 @@ namespace netdemo
             {
                 Console.WriteLine("Thong tin nhan vien truoc khi xoa:");
                 employee.ShowInfo();
+
+                // Xóa chứng chỉ liên quan nếu có
+                if (employee is Experience experience)
+                {
+                    certificates.RemoveAll(c => c.CretificatedID == experience.ID);
+                }
+                else if (employee is Fresher fresher)
+                {
+                    certificates.RemoveAll(c => c.CretificatedID == fresher.ID);
+                }
+                else if (employee is Intern intern)
+                {
+                    certificates.RemoveAll(c => c.CretificatedID == intern.ID);
+                }
 
                 employees.Remove(employee);
                 Console.WriteLine("Xoa thanh cong.");
@@ -177,23 +202,105 @@ namespace netdemo
                 employee.ShowInfo();
 
                 Console.WriteLine("Nhạp thong tin moi:");
+                if (employee is Experience)
+                {
+                    Experience experience = (Experience)employee;
+
+                    Console.Write("ExpInYear moi: ");
+                    string expInYearMoi = Console.ReadLine();
+                    experience.ExpInYear = expInYearMoi;
+
+                    Console.Write("ProSkill moi: ");
+                    string proSkillMoi = Console.ReadLine();
+                    experience.ProSkill = proSkillMoi;
+                }
+                else if (employee is Fresher)
+                {
+                    Fresher fresher = (Fresher)employee;
+
+                    Console.Write("GraduationDate moi: ");
+                    string graduationDateMoi = Console.ReadLine();
+                    if (DateTime.TryParse(graduationDateMoi, out DateTime validGraduationDate))
+                    {
+                        fresher.GraduationDate = validGraduationDate.ToString("dd/MM/yyyy");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ngay thang nam khong hop le. Sua thong tin that bai.");
+                        return;
+                    }
+
+
+                    Console.Write("GraduationRank moi: ");
+                    string graduationRankMoi = Console.ReadLine();
+                    fresher.GraduationRank = graduationRankMoi;
+
+                    Console.Write("Education moi: ");
+                    string educationMoi = Console.ReadLine();
+                    fresher.Education = educationMoi;
+                }
+                else if (employee is Intern)
+                {
+                    Intern intern = (Intern)employee;
+
+                    Console.Write("MaJors moi: ");
+                    string maJorsMoi = Console.ReadLine();
+                    intern.MaJors = maJorsMoi;
+
+                    Console.Write("SemeSter moi: ");
+                    string semeSterMoi = Console.ReadLine();
+                    intern.SemeSter = semeSterMoi;
+
+                    Console.Write("UniversityName moi: ");
+                    string universityNameMoi = Console.ReadLine();
+                    intern.UniversityName = universityNameMoi;
+                }
 
                 Console.Write("Ten moi: ");
                 string tenMoi = Console.ReadLine();
-                employee.FullName = tenMoi;
-
+                if (employee.IsValidFullName(tenMoi))
+                {
+                    employee.FullName = tenMoi;
+                }
+                else
+                {
+                    Console.WriteLine("Ten khong hop le. Sua thong tin that bai.");
+                    return;
+                }
                 Console.Write("Ngay sinh moi: ");
-                int ngaySinhMoi = int.Parse(Console.ReadLine());
-                employee.BirthDay = ngaySinhMoi;
+                int ngaySinhMoi;
+                if (int.TryParse(Console.ReadLine(), out ngaySinhMoi) && employee.IsValidBirthDay(ngaySinhMoi))
+                {
+                    employee.BirthDay = ngaySinhMoi;
+                }
+                else
+                {
+                    Console.WriteLine("Ngay sinh khong hop le. Sua thong tin that bai.");
+                    return;
+                }
 
                 Console.Write("So dien thoai moi: ");
                 string sdtMoi = Console.ReadLine();
-                employee.Phone = sdtMoi;
-
+                if (employee.IsValidPhone(sdtMoi))
+                {
+                    employee.Phone = sdtMoi;
+                }
+                else
+                {
+                    Console.WriteLine("So dien thoai khong hop le. Sua thong tin that bai.");
+                    return;
+                }
                 Console.Write("Email moi: ");
                 string emailMoi = Console.ReadLine();
-                employee.Email = emailMoi;
-
+                if (employee.IsValidEmail(emailMoi))
+                {
+                    employee.Email = emailMoi;
+                }
+                else
+                {
+                    Console.WriteLine("Email khong hop le. Sua thong tin that bai.");
+                    return;
+                }
                 Console.WriteLine("Thong tin nhan vien sau khi sua:");
                 employee.ShowInfo();
             }
@@ -233,20 +340,145 @@ namespace netdemo
     {
         public static void Main()
         {
-            Experience experience = new Experience(1, "Nguyen Van A", 1999, "0987654321", "XXXXXXXXXXXXXXXXXXXX", "FullTime", "1", "1", "HTML, CSS, JS");
-            experience.ShowInfo();
-
-            Fresher fresher = new Fresher(2, "Nguyen Van B", 2000, "0987654321", "XXXXXXXXXXXXXXXXXXXX", "PartTime", "1", "12/12/2022", "Xuat sac", "Hoc sinh");
-            fresher.ShowInfo();
-
-            Intern intern = new Intern(3, "Nguyen Van C", 2001, "0987654321", "XXXXXXXXXXXXXXXXXXXX", "Intern", "1", "IT", "1", "HUST");
-            intern.ShowInfo();
-
             BoPhanTuyenDung boPhanTuyenDung = new BoPhanTuyenDung();
+
+            Experience experience = new Experience(1, "Nguyen Van A", 1999, "0987654321", "test@gmail.com", "FullTime", "1", "1", "HTML, CSS, JS");
+            boPhanTuyenDung.employees.Add(experience);
+
+            Fresher fresher = new Fresher(2, "Nguyen Van B", 2000, "0987654321", "test2@gmail.com", "PartTime", "1", "12/12/2022", "Xuat sac", "Hoc sinh");
+            boPhanTuyenDung.employees.Add(fresher);
+
+            Intern intern = new Intern(3, "Nguyen Van C", 2001, "0987654321", "test3@gmail.com", "Intern", "1", "IT", "1", "HUST");
+            boPhanTuyenDung.employees.Add(intern);
+
             Certificate certificate = new Certificate(1, "HTML", "Xuat sac", "12/12/2022");
             boPhanTuyenDung.AddCertificate(certificate);
-            boPhanTuyenDung.XoaNhanVien(1);
-            boPhanTuyenDung.SuaThongTinNhanVien(2);
+
+            while (true)
+            {
+                Console.WriteLine("========== MENU ==========");
+                Console.WriteLine("1. Them nhan vien moi");
+                Console.WriteLine("2. Xoa nhan vien");
+                Console.WriteLine("3. Sua thong tin nhan vien");
+                Console.WriteLine("4. Hien thi thong tin nhan vien");
+                Console.WriteLine("0. Thoat");
+
+                Console.Write("Chon chuc nang (0-4): ");
+                int choice;
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    // Xử lý lựa chọn người dùng
+                    switch (choice)
+                    {
+                        case 1:
+                            // Thêm nhân viên mới
+                            Console.WriteLine("Chon loai nhan vien moi (1: Experience, 2: Fresher, 3: Intern): ");
+                            int typeChoice = int.Parse(Console.ReadLine());
+                            switch (typeChoice)
+                            {
+                                case 1:
+                                // Nhập thông tin cho nhân viên kinh nghiệm
+                                    Console.Write("Nhap ExpInYear: ");
+                                    string expInYear = Console.ReadLine();
+                                    Console.Write("Nhap ProSkill: ");
+                                    string proSkill = Console.ReadLine();
+                                    boPhanTuyenDung.employees.Add(new Experience(boPhanTuyenDung.employees.Count + 1, "New Employee", 2000, "123456789", "newemployee@example.com", "FullTime", "1", expInYear, proSkill));
+                                    break;
+
+                                case 2:
+                                // Nhập thông tin cho nhân viên mới tốt nghiệp
+                                    Console.Write("Nhap GraduationDate: ");
+                                    string graduationDate = Console.ReadLine();
+                                    Console.Write("Nhap GraduationRank: ");
+                                    string graduationRank = Console.ReadLine();
+                                    Console.Write("Nhap Education: ");
+                                    string education = Console.ReadLine();
+                                    boPhanTuyenDung.employees.Add(new Fresher(boPhanTuyenDung.employees.Count + 1, "New Employee", 2000, "123456789", "newemployee@example.com", "PartTime", "1", graduationDate, graduationRank, education));
+                                    break;
+
+                                case 3:
+                                // Nhập thông tin cho thực tập sinh
+                                    Console.Write("Nhap MaJors: ");
+                                    string maJors = Console.ReadLine();
+                                    Console.Write("Nhap SemeSter: ");
+                                    string semeSter = Console.ReadLine();
+                                    Console.Write("Nhap UniversityName: ");
+                                    string universityName = Console.ReadLine();
+                                    boPhanTuyenDung.employees.Add(new Intern(boPhanTuyenDung.employees.Count + 1, "New Employee", 2000, "123456789", "newemployee@example.com", "Intern", "1", maJors, semeSter, universityName));
+                                    break;
+                                default:
+                                    Console.WriteLine("Lua chon khong hop le");
+                                    break;
+                            }
+                            break;
+
+                        case 2:
+                        // Xóa nhân viên
+                            Console.Write("Nhap ID nhan vien can xoa: ");
+                            int idToDelete;
+                            if (int.TryParse(Console.ReadLine(), out idToDelete))
+                            {
+                                boPhanTuyenDung.XoaNhanVien(idToDelete);
+                            }
+                            else
+                            {
+                                Console.WriteLine("ID khong hop le.");
+                            }
+                            break;
+
+                        case 3:
+                        // Sửa thông tin nhân viên
+                            Console.Write("Nhap ID nhan vien can sua: ");
+                            int idToEdit;
+                            if (int.TryParse(Console.ReadLine(), out idToEdit))
+                            {
+                                boPhanTuyenDung.SuaThongTinNhanVien(idToEdit);
+                            }
+                            else
+                            {
+                                Console.WriteLine("ID khong hop le.");
+                            }
+                            break;
+
+                        case 4:
+                        // Hiển thị thông tin nhân viên
+                            Console.Write("Nhap ID nhan vien can hien thi thong tin: ");
+                            int idToDisplay;
+                            if (int.TryParse(Console.ReadLine(), out idToDisplay))
+                            {
+                                Employee employeeToDisplay = boPhanTuyenDung.employees.FirstOrDefault(e => e.ID == idToDisplay);
+                                if (employeeToDisplay != null)
+                                {
+                                    Console.WriteLine("========== THONG TIN NHAN VIEN ==========");
+                                    employeeToDisplay.ShowInfo();
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"Khong tim thay nhan vien co ID {idToDisplay}");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("ID khong hop le.");
+                            }
+                            break;
+
+                        case 0:
+                            Console.WriteLine("Thoat chuong trinh.");
+                            return;
+
+                        default:
+                            Console.WriteLine("Lua chon khong hop le");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Lua chon khong hop le");
+                }
+
+                Console.WriteLine(); 
+            }
         }
     }
 }
